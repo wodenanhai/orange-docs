12246132f218696f309cff2217133a07dbcbc3a5
zhangcheng <2778162192@qq.com>
2025-11-27 15:20:19 +0800

    commit

---
+++ docs/.vuepress/.temp/pages/node/createDateBase.html.vue
@@ -0,0 +1,15 @@
+<template><div><h4 id="如何创建数据库与数据表" tabindex="-1"><a class="header-anchor" href="#如何创建数据库与数据表"><span>如何创建数据库与数据表？</span></a></h4>
+<p>操作数据库：增删改查（Create、Delete、Update、Read），CURD
+数据库编码选择 utf8mb4，是一种能非常好的支持世界各国语言的编码。后面的mb4可以让数据库正确的存储emoji表情符号
+排序规则的选择 utf8mb4_general_ci，区分大小写，不区分重音符号。
+创建表：</p>
+<ul>
+<li>首字母大写：这里的首字母需要大写，如果你命名成小写了，将来部署到Linux服务器会报错的。</li>
+<li>复数形式：最后的s，这个复数形式，也是必须的。如果你的命名掉了s，在后面我们使用Node.js查询数据的时候，它也会无法找到这张表的。</li>
+</ul>
+<h4 id="mysql-中常见的数据类型" tabindex="-1"><a class="header-anchor" href="#mysql-中常见的数据类型"><span>MySQL 中常见的数据类型</span></a></h4>
+<h4 id="主键是什么" tabindex="-1"><a class="header-anchor" href="#主键是什么"><span>主键是什么？</span></a></h4>
+<h4 id="给数据表-增加标题和内容" tabindex="-1"><a class="header-anchor" href="#给数据表-增加标题和内容"><span>给数据表，增加标题和内容</span></a></h4>
+</div></template>
+
+
---
+++ docs/.vuepress/.temp/pages/node/docker_mysql.html.vue
@@ -0,0 +1,28 @@
+<template><div><p>进入<a href="https://docker.com/get-started/" target="_blank" rel="noopener noreferrer">docker官网</a>下载
+安装好之后，设置中Docker Engine配置中国镜像</p>
+<div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre v-pre><code class="language-text"><span class="line"> "registry-mirrors": [</span>
+<span class="line">    "https://docker.1ms.run"</span>
+<span class="line">]</span>
+<span class="line"></span></code></pre>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>接着进入项目根目录中，新建一个文件，叫做docker-compose.yml。千万要注意，一定要在项目根目录中，放在其他地方会找不到的。然后将下面的配置复制进去，这就是MySQL的一个简单配置了。</p>
+<div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre v-pre><code class="language-text"><span class="line">services:</span>
+<span class="line">  mysql:</span>
+<span class="line">    image: mysql:8.3.0</span>
+<span class="line">    command:</span>
+<span class="line">      --default-authentication-plugin=mysql_native_password</span>
+<span class="line">      --character-set-server=utf8mb4</span>
+<span class="line">      --collation-server=utf8mb4_general_ci</span>
+<span class="line">    environment:</span>
+<span class="line">      - MYSQL_ROOT_PASSWORD=clwy1234</span>
+<span class="line">      - MYSQL_LOWER_CASE_TABLE_NAMES=0</span>
+<span class="line">    ports:</span>
+<span class="line">      - "3306:3306"</span>
+<span class="line">    volumes:</span>
+<span class="line">      - ./data/mysql:/var/lib/mysql</span>
+<span class="line"></span></code></pre>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>然后再运行下面命令，这样，MySQL就会自动下载好，并启动起来了。</p>
+<div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre v-pre><code class="language-text"><span class="line">docker-compose up -d</span>
+<span class="line"></span></code></pre>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div></div></div></div></template>
+
+
