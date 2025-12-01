a4f2126d43ee08e300b897a1d9b2f825c25590d0
zhangcheng <2778162192@qq.com>
2025-12-01 13:52:33 +0800

    commit

---
+++ docs/.vuepress/.temp/pages/node/curd.html.vue
@@ -0,0 +1,5 @@
+<template><div><h4 id="_1-如何使用sql语句实现增加、修改和删除" tabindex="-1"><a class="header-anchor" href="#_1-如何使用sql语句实现增加、修改和删除"><span>1.如何使用SQL语句实现增加、修改和删除？</span></a></h4>
+<h4 id="_2-sql语句的语法规则" tabindex="-1"><a class="header-anchor" href="#_2-sql语句的语法规则"><span>2.SQL语句的语法规则</span></a></h4>
+</div></template>
+
+
