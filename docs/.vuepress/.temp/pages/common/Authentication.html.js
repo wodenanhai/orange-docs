4f5b19822c23e54b7536ef0898ef35a382ad9787
zhangcheng <2778162192@qq.com>
2025-12-01 13:22:08 +0800

    commit

---
+++ docs/.vuepress/.temp/pages/common/Authentication.html.js
@@ -0,0 +1,16 @@
+import comp from "/Users/zhangcheng/Desktop/All_In_One/orange-docs/docs/.vuepress/.temp/pages/common/Authentication.html.vue"
+const data = JSON.parse("{\"path\":\"/common/Authentication.html\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"git\":{},\"filePathRelative\":\"common/Authentication.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
---
+++ docs/.vuepress/.temp/pages/common/Authentication.html.vue
@@ -0,0 +1,10 @@
+<template><div><h4 id="常用鉴权方式" tabindex="-1"><a class="header-anchor" href="#常用鉴权方式"><span>常用鉴权方式</span></a></h4>
+<ol>
+<li>HTTP Basic Authentication(HTTP基本认证)</li>
+<li>session-cookie</li>
+<li>Token验证（包括JWT，SSO）</li>
+<li>OAuth（开放授权）</li>
+</ol>
+</div></template>
+
+
