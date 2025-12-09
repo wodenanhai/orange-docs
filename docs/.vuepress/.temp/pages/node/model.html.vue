b01fa89e28549583f1d6cbda29e3b12f483c9eb3
zhangcheng <2778162192@qq.com>
2025-12-09 14:33:36 +0800

    commit

---
+++ docs/.vuepress/.temp/pages/node/model.html.vue
@@ -0,0 +1,5 @@
+<template><div><h4 id="课程内容sql部分不进行记录了-因为不会使用sequelize的方式操作数据库" tabindex="-1"><a class="header-anchor" href="#课程内容sql部分不进行记录了-因为不会使用sequelize的方式操作数据库"><span>课程内容sql部分不进行记录了，因为不会使用sequelize的方式操作数据库</span></a></h4>
+<p><a href="https://clwy.cn/chapters/fullstack-node-migration" target="_blank" rel="noopener noreferrer">若有需要直接查看</a></p>
+</div></template>
+
+
