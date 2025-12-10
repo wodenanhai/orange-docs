8c4a966216d8e585fc6adfefe2056c78b503e6be
zhangcheng <2778162192@qq.com>
2025-12-10 11:58:53 +0800

    commit

---
+++ docs/.vuepress/.temp/pages/node/api8.html.vue
@@ -0,0 +1,9 @@
+<template><div><div class="language-javascript line-numbers-mode" data-highlighter="prismjs" data-ext="js"><pre v-pre><code class="language-javascript"><span class="line"><span class="token keyword">const</span> body <span class="token operator">=</span> <span class="token punctuation">{</span></span>
+<span class="line">    <span class="token literal-property property">title</span><span class="token operator">:</span> req<span class="token punctuation">.</span>body<span class="token punctuation">.</span>title<span class="token punctuation">,</span></span>
+<span class="line">    <span class="token literal-property property">content</span><span class="token operator">:</span> req<span class="token punctuation">.</span>body<span class="token punctuation">.</span>content</span>
+<span class="line"><span class="token punctuation">}</span></span>
+<span class="line"></span></code></pre>
+<div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="而不是直接接收全部的req-body值" tabindex="-1"><a class="header-anchor" href="#而不是直接接收全部的req-body值"><span>而不是直接接收全部的req.body值</span></a></h4>
+</div></template>
+
+
