7d15420df7a797eb78cd920ad6e3ae794f090b06
zhangcheng <2778162192@qq.com>
2025-12-10 14:11:22 +0800

    commit

--- docs/.vuepress/.temp/pages/node/api9.html.js
+++ docs/.vuepress/.temp/pages/node/api9.html.js
@@ -1,5 +1,5 @@
 import comp from "/Users/zhangcheng/Desktop/All_In_One/orange-docs/docs/.vuepress/.temp/pages/node/api9.html.vue"
-const data = JSON.parse("{\"path\":\"/node/api9.html\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"git\":{},\"filePathRelative\":\"node/api9.md\"}")
+const data = JSON.parse("{\"path\":\"/node/api9.html\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"git\":{\"updatedTime\":1765346101000,\"contributors\":[{\"name\":\"zhangcheng\",\"username\":\"zhangcheng\",\"email\":\"2778162192@qq.com\",\"commits\":1,\"url\":\"https://github.com/zhangcheng\"}],\"changelog\":[{\"hash\":\"eca64241d5bcc7b612014cc3756100f33b3f1a34\",\"time\":1765346101000,\"email\":\"2778162192@qq.com\",\"author\":\"zhangcheng\",\"message\":\"commit\"}]},\"filePathRelative\":\"node/api9.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
--- docs/node/api9.md
+++ docs/node/api9.md
@@ -56,3 +56,30 @@ function success(res, message, data = {}, code = 200) {
 | 请求参数不存在 | 400 | 数据验证失败，而且还需要map遍历错误信息 |
 | 数据不存在   | 404 | 查询不存在的内容              |
 | 服务器错误   | 500 | 未知的各种问题               |
+
+```javascript
+function failure(res, error) {
+    if (error.name === 'SequelizeValidationError') {
+        const errors = error.errors.map(e => e.message);
+        return res.status(400).json({
+            status: false,
+            message: "请求参数错误",
+            errors
+        })
+    }
+    if (error.name === 'NotFoundError') {
+        return res.status(404).json({
+            status: false,
+            message: "资源不存在",
+            errors: [error.message]
+        })
+    }
+    return res.status(500).json({
+        status: false,
+        message: "服务器错误",
+        errors: [error.message]
+    })
+}
+```
+
+#### 也可以使用错误中间件
